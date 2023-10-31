196bcd0a23b1e3d74be5166cc1f71ebcb3089d52
Mikhail Korol <mishakorol101@gmail.com>
2023-10-31 17:36:00 +0200

    refactor

---
+++ commands/list.php
@@ -0,0 +1,14 @@
+<?php
+function listCommand(array $arguments)
+{
+	$todos = getTodosOrFail();
+
+	foreach ($todos as $index => $todo) {
+		echo sprintf(
+			"%s. [%s]  %s \n",
+			($index + 1),
+			$todo['completed'] ? 'X' : ' ',
+			$todo['title']
+		);
+	}
+}
--- todo.php
+++ todo.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . '/boot.php';
 function main(array $arguments):void
 {
 	array_shift($arguments);
@@ -12,8 +13,11 @@ function main(array $arguments):void
 		case 'add':
 			addCommand($arguments);
 			break;
-		case 'complete':
-			completeCommand($arguments);
+		case 'done':
+			doneCommand($arguments);
+			break;
+		case 'undone':
+			undoneCommand($arguments);
 			break;
 		case 'remove':
 		case 'rm':
@@ -27,83 +31,6 @@ function main(array $arguments):void
 			exit(1);
 	}
 	exit(0);
-}
-
-function reportCommand(array $arguments)
-{
-
-}
-
-function removeCommand(array $arguments)
-{
-
-}
-
-function completeCommand(array $arguments)
-{
-
-}
-
-function addCommand(array $arguments)
-{
-	$title = array_shift($arguments);
-
-	$todo = [
-		'id' => uniqid(),
-		'title' => $title,
-		'completed' => false,
-	];
-
-	$fileName = date('Y-m-d') . '.txt';
-	$filePath = __DIR__ . '/data/' . $fileName;
-
-	if (file_exists($filePath)){
-		$content = file_get_contents($filePath);
-		$todos = unserialize($content, [
-			'allowed_classes' => false,
-		]);
-		$todos[] = $todo;
-		file_put_contents($filePath, serialize($todos));
-	}
-	else
-	{
-		$todos = [$todo];
-		file_put_contents($filePath, serialize($todos));
-	}
-
-	file_put_contents($filePath, $title . "\n", FILE_APPEND);
-}
-
-function listCommand(array $arguments)
-{
-	$fileName = date('Y-m-d') . '.txt';
-	$filePath = __DIR__ . '/data/' . $fileName;
-	if (!file_exists($filePath))
-	{
-		echo 'Nothing to do here';
-		return;
-	}
-
-	$content = file_get_contents($filePath);
-	$todos = unserialize($content, [
-		'allowed_classes' => false,
-	]);
-
-	if(empty($todos))
-	{
-		echo 'Nothing to do here';
-		return;
-	}
-
-	foreach ($todos as $index => $todo)
-	{
-		echo sprintf(
-			"%s. [%s]  %s \n",
-			($index+1) ,
-			$todo['completed'] ? 'X' : ' ',
-			$todo['title']
-		);
-	}
 }
 
 main($argv);
